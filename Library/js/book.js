175649b2893687624c750bf6fb69e08166530714
Amor Osmic <amorosmic@gmx.com>
2019-04-22 15:14:31 +0200

    Created a table with books info and added a button to add new books

--- Library/js/book.js
+++ Library/js/book.js
@@ -1,4 +1,7 @@
 let myLibrary = [];
+let docTable = document.querySelector("#books");
+
+document.querySelector(".button").addEventListener("click", createBook);
 
 function Book(title, author, numberOfPages, readBook) {
     this.title = title;
@@ -20,6 +23,26 @@ function addBookToLibrary(title, author, numberOfPages, readBook) {
     myLibrary[librarySize] = new Book(title, author, numberOfPages, readBook);
 }
 
-addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 259, false);
-console.log(myLibrary[0].info());
+function render() {
+    myLibrary.forEach(book => {
+        let row = document.createElement("tr");
 
+        Object.values(book).forEach(value => {
+            let cell = document.createElement("td");
+            let cellText = document.createTextNode(value);
+
+            cell.appendChild(cellText);
+            row.appendChild(cell);
+        });
+        docTable.appendChild(row);
+    });
+}
+
+function createBook() {
+    const title = prompt("Enter the title of the book", "Title");
+    const author = prompt("Enter the author of the book", "Author");
+    const numberOfPages = prompt("Enter the number of the pages of the book", 0);
+
+    addBookToLibrary(title, author, numberOfPages, false);
+    render();
+}
