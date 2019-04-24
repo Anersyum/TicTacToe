65f8ef9a6bf2fac8aa8d2cf2c3b8a76031b319f9
Amor Osmic <amorosmic@gmx.com>
2019-04-24 10:48:13 +0200

    Restricted play on already played field

--- TicTacToeJs/js/game.js
+++ TicTacToeJs/js/game.js
@@ -2,8 +2,8 @@ const fields = document.querySelectorAll("button");
 const manager = gameManager();
 
 function addEventListenerToButtons() {
-    fields.forEach(button => {
-        button.addEventListener("click", changeState);
+    fields.forEach(field => {
+        field.addEventListener("click", changeState);
     })
 }
 
@@ -21,18 +21,21 @@ function player(symbol) {
     const getPlayerSymbol = () => symbol;
     return {getPlayerSymbol};
 }
-
-(function changeState() {
+function changeState() {
     const player1 = player("X");
     const player2 = player("O");
 
     if (manager.getTurn() === 0) {
-        manager.changeTurn();
-        this.textContent = player1.getPlayerSymbol();
+        if (this.textContent === "") {
+            manager.changeTurn();
+            this.textContent = player1.getPlayerSymbol();
+        }
     } else {
-        manager.changeTurn();
-        this.textContent = player2.getPlayerSymbol();
+        if (this.textContent === "") {
+            manager.changeTurn();
+            this.textContent = player2.getPlayerSymbol();
+        }
     }
-})
+}
 
 addEventListenerToButtons();
