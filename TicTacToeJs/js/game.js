c2f4cdf9224efaf610036baac7b0caa6d5f311b5
Amor Osmic <amorosmic@gmx.com>
2019-04-24 11:19:58 +0200

    Created winning condition

--- TicTacToeJs/js/game.js
+++ TicTacToeJs/js/game.js
@@ -17,25 +17,73 @@ function gameManager() {
     return {changeTurn, getTurn};
 }
 
-function player(symbol) {
+function player(symbol, name) {
     const getPlayerSymbol = () => symbol;
-    return {getPlayerSymbol};
+    const getPlayerName = () => name;
+    return {getPlayerSymbol, getPlayerName};
 }
 function changeState() {
-    const player1 = player("X");
-    const player2 = player("O");
-
-    if (manager.getTurn() === 0) {
-        if (this.textContent === "") {
-            manager.changeTurn();
-            this.textContent = player1.getPlayerSymbol();
-        }
-    } else {
-        if (this.textContent === "") {
-            manager.changeTurn();
-            this.textContent = player2.getPlayerSymbol();
-        }
+    const player1 = player("X", "Amor");
+    const player2 = player("O", "Štahor");
+    const playerOnTurn = (manager.getTurn() === 0) ? player1 : player2;
+    
+    if (this.textContent === "") {
+        this.textContent = playerOnTurn.getPlayerSymbol();
     }
+    manager.changeTurn();
+    if (checkWinningCondition(playerOnTurn)){
+        document.querySelector("div").textContent = `${playerOnTurn.getPlayerName()} wins!`;
+    }
+}
+
+function checkWinningCondition(player) {
+    let counter = 0;
+
+    // horizontal matching
+    for (let i = 2; i < fields.length; i += 3) {
+        for (let j = i - 2; j <= i; j++) {
+            if (fields[j].textContent != player.getPlayerSymbol())
+                break;
+            counter++;
+        }
+        if (counter === 3)
+            return true;
+        else 
+            counter = 0;
+    }
+
+    // vertical matching
+    for (let i = 0; i < 3; i++) {
+        for (let j = i; j <= (fields.length + i - 2); j += 3) {
+            if (fields[j].textContent != player.getPlayerSymbol())
+                break;
+            counter++;
+        }
+        if (counter === 3)
+            return true;
+        else 
+            counter = 0;
+    }
+
+    // left diagonal matching
+    for (let i = 0; i < fields.length; i += 4) {
+        if (fields[i].textContent != player.getPlayerSymbol())
+            break;
+        counter++;
+    }
+    if (counter === 3)
+        return true;
+
+    // right diagonal matching
+    for (let i = 2; i < fields.length - 1; i += 2) {
+        if (fields[i].textContent != player.getPlayerSymbol())
+            break;
+        counter++;
+    }
+    if (counter === 3)
+        return true;
+    
+    return false;
 }
 
 addEventListenerToButtons();
